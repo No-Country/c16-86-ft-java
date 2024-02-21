e39f62f2fb2a30d0d2291edd17968a068ada7655
santiago nicolas perez kay <santiagoperezkay88@gmail.com>
2024-02-21 15:35:23 -0300

    cargando target oculto

---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO client (high, firstname, phone, secondname) VALUES ('2001-05-05','marina','01115232232','morana');
+INSERT INTO client (high, firstname, phone, secondname) VALUES ('2001-06-06','marina','01115232232','gutierrez');
+
+
+
