7fdcb28f46e1d5f44fcc1338c38776073313b833
xcastalax <castala.it@gmail.com>
2024-02-22 02:09:18 -0300

    Estructura de carpetas de modales

---
+++ FrontEnd/src/components/Modales/Modal Crear Parker/ModalParker.jsx
@@ -0,0 +1,9 @@
+function ModalParker() {
+    return (
+    <div className="px-5 py-1 sm:px-6 sm:py-2 self-end border shadow-sm text-center">
+        <h1 className="bg-red-600"> Modal Crear Parker</h1>
+    </div>
+    )
+}
+
+export default ModalParker
