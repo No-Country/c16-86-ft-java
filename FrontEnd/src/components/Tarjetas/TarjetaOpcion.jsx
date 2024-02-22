7203b26bfb728b64827f93a2551a341075962308
Jaime A <jaimeangel1097@gmail.com>
2024-02-21 22:24:45 -0500

    cambio en size componente TarjetaOpcion

--- FrontEnd/src/components/Tarjetas/TarjetaOpcion.jsx
+++ FrontEnd/src/components/Tarjetas/TarjetaOpcion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TarjetaOpcion({text}) {
   return (
-    <div className='w-full max-w-sm sm:min-h-28 xl:min-h-0  bg-white border shadow-md px-5 py-4 text-center sm:py-6'>
+    <div className='w-full max-w-sm sm:min-h-28 xl:min-h-32  bg-white border shadow-md px-5 py-4 text-center sm:py-6'>
       <h1 className='font-semibold text-lg xl:text-xl'>{text}</h1>
     </div>
   )
