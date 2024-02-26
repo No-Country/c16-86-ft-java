d1893260cf94984d80ffab45f4c874cc2e1df91e
Jaime A <jaimeangel1097@gmail.com>
2024-02-26 01:21:58 -0500

    bug UI dashboard contenido

--- FrontEnd/src/pages/DashboardSecundario.jsx
+++ FrontEnd/src/pages/DashboardSecundario.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 function DashboardSecundario() {
     
     return (
-        <div className="w-full flex flex-col sm:flex-row sm:gap-5 sm:px-5 sm:py-5 bg-slate-50">
+        <div className="w-full flex flex-col h-screen sm:flex-row sm:gap-5 sm:px-5 sm:py-5 bg-slate-50">
             
             <div className="w-full sm:hidden">
                 <NavBar/>
@@ -15,7 +15,7 @@ function DashboardSecundario() {
                 <SideBar/>
             </div>
 
-            <div className="w-full sm:w-8/12 xl:w-9/12 bg-white rounded-lg shadow-2xl px-5 pt-5">
+            <div className="w-full overflow-scroll h-full sm:w-8/12 xl:w-9/12 bg-white rounded-lg shadow-2xl px-5 py-5">
                 <div className="w-full hidden sm:block text-center">
                     <h1 className="text-3xl xl:text-4xl font-bold">ParkingOut Ectasis</h1>
                 </div>
