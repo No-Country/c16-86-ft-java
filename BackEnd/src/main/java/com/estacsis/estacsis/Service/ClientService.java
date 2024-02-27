a31230b1b6001afe1b5096a1b662029c8b724949
santiagoperezkay <santiagoperezkay88@gmail.com>
2024-02-27 17:21:30 -0300

    modificacion de atributo private de clientRepository

--- BackEnd/src/main/java/com/estacsis/estacsis/Service/ClientService.java
+++ BackEnd/src/main/java/com/estacsis/estacsis/Service/ClientService.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 @Service
 public class ClientService {
     @Autowired
-    private ClientRepository clientRepository;
+   ClientRepository clientRepository;
 
 
     /**
