b2f9f8f1d123899370bc1f8e1f7ef8fe3f69fc2b
santiagoperezkay <santiagoperezkay88@gmail.com>
2024-02-27 17:46:44 -0300

    agregando anotacion @ComponentSCan

--- BackEnd/src/main/java/com/estacsis/EstacsisApplication.java
+++ BackEnd/src/main/java/com/estacsis/EstacsisApplication.java
@@ -2,8 +2,10 @@ package com.estacsis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.estacsis"})
 public class EstacsisApplication {
 
 	public static void main(String[] args) {
