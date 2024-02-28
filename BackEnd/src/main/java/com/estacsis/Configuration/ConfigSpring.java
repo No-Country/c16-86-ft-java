cd9edadc387d139eac28e7c75e24287bbe7f9101
santiagoperezkay <santiagoperezkay88@gmail.com>
2024-02-27 23:13:02 -0300

    configuration class

---
+++ BackEnd/src/main/java/com/estacsis/Configuration/ConfigSpring.java
@@ -0,0 +1,12 @@
+package com.estacsis.Configuration;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+
+
+public class ConfigSpring {
+}
