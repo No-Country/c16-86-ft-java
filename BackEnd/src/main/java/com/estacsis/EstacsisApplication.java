e5a8cae53eccfce9c18b49e3bd533222221b6a0c
santiagoperezkay <santiagoperezkay88@gmail.com>
2024-02-27 23:17:38 -0300

    configuration class v1.1.1

--- BackEnd/src/main/java/com/estacsis/EstacsisApplication.java
+++ BackEnd/src/main/java/com/estacsis/EstacsisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 
 
+@ComponentScan(basePackages = {"com.estacsis.service", "com.estacsis.repository", "com.estacsis.controller", "com.estacsis.entitie"})
 public class EstacsisApplication {
 
 	public static void main(String[] args) {
