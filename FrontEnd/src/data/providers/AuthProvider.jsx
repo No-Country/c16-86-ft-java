1e5bc794d2a58a912617c68c8784926d4e46fe61
Jaime A <jaimeangel1097@gmail.com>
2024-03-02 23:49:02 -0500

    creando estructura para el manejo del estado de la app usando Context/Custom hooks

--- FrontEnd/src/data/providers/AuthProvider.jsx
+++ FrontEnd/src/data/providers/AuthProvider.jsx
@@ -1,12 +1,38 @@
+import { useState,useEffect } from "react";
+
 import { createContext } from "react";
 
 const AuthContext = createContext()
 
 function AuthProvider({children}) {
+    const [auth,setAuth]=useState('')
+
+    useEffect(()=>{
+        const authUser= async ()=>{
+            const URL_verificar_tipo_usuario=''
+
+
+            const configuracion = {
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            }
+
+            try {
+                const {data} = await axios(URL_verificar_tipo_usuario,configuracion)
+                setAuth(data)
+            }catch(error) {
+                console.log(error)
+            }
+        }
+        authUser()
+    },[])
+
+    
     return (
         <AuthContext.Provider
             value={{
-
+                auth
             }}
         >
             {children}
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+//provider
+import AuthProvider from './data/providers/AuthProvider.jsx'
+import ColaboradorProvider from './data/providers/ColaboradorProvider.jsx'
+import EstacionamientoProvider from './data/providers/EstacionamientoProvider.jsx'
+import TicketProvider from './data/providers/TicketProvider.jsx'
 //react router
 import {RouterProvider} from 'react-router-dom'
 //routes
 import router from './routes/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider
-    router={router}
-  >
-  </RouterProvider>,
+  <AuthProvider>
+    <EstacionamientoProvider>
+      <ColaboradorProvider>
+        <TicketProvider>
+          <RouterProvider
+            router={router}
+          >
+          </RouterProvider>
+        </TicketProvider>
+      </ColaboradorProvider>
+    </EstacionamientoProvider>
+  </AuthProvider>
 )
