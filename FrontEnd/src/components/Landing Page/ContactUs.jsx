23a339b8e34c415c76baecef72eb9d6d0bd9d238
Jaime A <jaimeangel1097@gmail.com>
2024-03-05 10:48:20 -0500

    cambios header/main agregando nuevo componente contactanos

---
+++ FrontEnd/src/components/Landing Page/ContactUs.jsx
@@ -0,0 +1,31 @@
+import contactus from '../../../public/contact_us_banner.jpg'
+
+function ContactUs() {
+    return (
+        <div className="flex px-8 sm:px-16 lg:px-20 xl:px-28 lg:py-14 sm:py-12 py-10 flex-col sm:flex-row sm:gap-2 bg-gray-100">
+
+            <div className='sm:w-1/2'>
+                <div className="pb-5 xl:py-10 sm:py-5 sm:gap-2 flex flex-col xl:gap-4">
+                    <h1 className="xl:text-6xl lg:text-5xl text-4xl font-semibold">Empieza tu</h1>
+                    <h1 className="xl:text-6xl lg:text-5xl text-4xl font-semibold">Revolucion con</h1>
+                    <h1 className="xl:text-6xl lg:text-5xl text-4xl font-semibold">Nuestro software</h1>
+                </div>
+                <button
+                    className="py-2 px-8 text-2xl font-bold uppercase bg-slate-50 rounded-md border border-black"
+                >
+                    contáctanos
+                </button>
+            </div>
+
+            <div className='hidden sm:block sm:w-1/2'>
+                <img 
+                    src={contactus} 
+                    alt=""
+                    className='rounded' 
+                />
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs;
--- FrontEnd/src/pages/LandingPage.jsx
+++ FrontEnd/src/pages/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Navegador from '../components/Dashboard/Navegador'
 import Intro from '../components/Landing Page/Intro'
 import Servicios from '../components/Landing Page/NuestrosServicios'
+import ContactUs from '../components/Landing Page/ContactUs'
 
 import imagenBack from '../../public/pexels-pixabay-63294.jpg'
 
@@ -11,12 +12,13 @@ function LandingPage() {
       style={{
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.7)),url(${imagenBack})`
       }}
-      className="w-full bg-black xl:px-16 px-5 pt-8 pb-16 bg-no-repeat bg-cover bg-fixed"
+      className="w-full h-screen bg-black xl:px-16 px-5 pt-8 pb-16 bg-no-repeat bg-cover bg-fixed"
     >
       <Navegador/>
       <Intro/>
     </div>
     <Servicios/>
+    <ContactUs/>
   </div>
   )
 }
