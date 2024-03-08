ecf0f34c75b1ea19edfde8d39003031b62357d10
xcastalax <castala.it@gmail.com>
2024-03-08 04:14:50 -0300

    nuevos ajustes de NuestrosServicios

--- FrontEnd/src/components/Landing Page/NuestrosServicios.jsx
+++ FrontEnd/src/components/Landing Page/NuestrosServicios.jsx
@@ -8,27 +8,27 @@ const Card = () => {
         <div className='px-2 py-2 border shadow'>
             <img src={imagenBack1} className="rounded-md" />
             <div className='my-2'> 
-                <h2 className='font-semibold'>Administraciones de Estacionamientos</h2>
-                <p className='text-sm mt-1'>
-                Administra tus estacionamientos. 
+                <h2 className='text-center font-semibold'>Administra tu estacionamiento</h2>
+                <p className='text-center text-sm mt-1'>
+                Gestiona tu parking en tiempo real.
                     </p>  
             </div>
         </div>
         <div className='px-2 py-2 border shadow'>
             <img src={imagenBack2} className="rounded-md" />
             <div className='my-2'> 
-                <h2 className='font-semibold'>Registro de Entradas/Salidas</h2>
-                <p className='text-sm mt-1'>
-                Registra de forma eficiente las entradas/salidas de vehiculos. 
+                <h2 className='text-center font-semibold'>Registro de entradas/salidas</h2>
+                <p className='text-center text-sm mt-1'>
+                Registra de forma eficiente la entrada y salida de vehiculos.
                     </p>  
             </div>
         </div>
         <div className='px-2 py-2 border shadow'>
             <img src={imagenBack3} className="rounded-md" />
             <div className='my-2'> 
-                <h2 className='font-semibold'>Acceso a Empleados</h2>
-                <p className='text-sm mt-1'>
-                Brindale acceso a tus empleados.
+                <h2 className='text-center font-semibold'>Acceso a empleados</h2>
+                <p className='text-center text-sm mt-1'>
+                Brindale acceso inmediato a tus colaboradores.
                     </p>  
             </div>
         </div>
